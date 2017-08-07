19eaf755c1e3b64d99bbd27f20874ce5f9f7606a
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-08-06 21:23:17 -0400

    finished mobile nav for real this time

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 // Hamburger Menu Toggle
   $('.nav-toggle').click(function() {
     $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
   })
 
 })
