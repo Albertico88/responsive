3af58bb46f67cdc043d54fb66c480f53eb0f9e9c
Alberto Grau <alberto.grau.margarida@gmail.com>
2017-08-09 18:44:08 -0400

    more cleanup and fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var sass = require('gulp-sass');
 
 
 gulp.task('sass', function() {
-  gulp.src('./src/scss/main.scss')
+  gulp.src('./docs/scss/main.scss')
   .pipe(sass({includedPaths: ['scss']}))
-  .pipe(gulp.dest('./src/css'));
+  .pipe(gulp.dest('./docs/css'));
 });
 
 gulp.task('serve', function() {
-  browserSync.init(["./src/css/*.css"], {
+  browserSync.init(["./docs/css/*.css"], {
     server: {
       baseDir: "./docs"
     }
@@ -18,6 +18,6 @@ gulp.task('serve', function() {
 });
 
 gulp.task('default', ['sass', 'serve'], function() {
-  gulp.watch("./src/scss/**/*.scss", ['sass']);
+  gulp.watch("./docs/scss/**/*.scss", ['sass']);
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
